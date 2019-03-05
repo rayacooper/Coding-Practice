f2c09c56f0e306227616e488ced1c7c2550a9aa7
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-03-04 19:11:43 -0700

    More practice stuff; Possible bugs

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var me = {
     favoriteFood: "Sushi",
     weight: 140
 }
-console.log(me.name + " is a " + me.age + " year old woman who loves " + me.husband + " and " + me.favoriteFood + "!");
+console.log("${me.name} is a ${me.age} year old dog.");
 
 var testArray = [1, 2, [1, 2, 3]];
 console.log(testArray[2][0]);
@@ -47,3 +47,79 @@ function makeTheMenu() {
 
 makeTheMenu();
 console.log(menuOptions[6]);
+
+var friends = [
+    {
+        name: "Diana",
+        age: 34
+    },
+    {
+        name: `Someone`
+    }
+]
+
+var family = {
+    philip: {
+        hobbies: {
+
+        }
+    },
+    terrence: {
+        hobbies:{
+
+        }
+    },
+    uglyBob: {
+        hobbies:{
+
+        }
+    }
+}
+var Sonya = {
+    ageInMonths: 6,
+    hairColor: "Blonde",
+    isDiaperPoopy: false,
+    percentileInHeight: 90,
+    isCute: true,
+    favFoods: ["Banana", "Plum Oatmeal", "Nutella"]
+};
+
+var alligatorPear = {
+    skin = 'bumpy'
+};
+
+/// moi
+const Sarah = {
+    height: "5'5\"",
+    weight: 162,
+    eyeColor: "Hazel",
+    age: 29,
+    birthday: "01/14/1990",
+    email: `sarah.jkr@gmail.com`,
+    isMale: false,
+    livesInUsa: true,
+    address: {
+        street: `942 Sage Creek Circle`,
+        city: `Springville`,
+        state: `Utah`,
+        zipCode: `84663`
+    },
+    favoriteFoods: [`Sushi`, `Bacon`, `Bananas`]
+}
+
+console.log(Sarah.address.street);
+
+var stuffToDo = [
+    function doTheThing() {
+        return `Thing`;
+    },
+    function eatTheBacon(){
+        return `Yummy`;
+    },
+    function kissYoHusband(){
+        return `Aww Yeah`;
+    }
+]
+
+console.log(stuffToDo[0]);
+
