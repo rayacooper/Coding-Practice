53d32fd45999b72a7242f6facc5f67bfb4dfccc1
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-03-04 19:11:00 -0700

    New Practice File

---
+++ newPractice.js
@@ -0,0 +1,13 @@
+const Sarah = {
+    age: 29,
+    height: `5'5"`,
+    weight: 162,
+    isMale: false,
+    favoriteFoods: [`Sushi`, `Bananas`, `Bacon`],
+    address: {
+        street: `942 Sage Creek Circle`,
+        city: `Springville`,
+        state:`Utah`
+    }
+}
+Sarah.favoriteFoods[1];
