3d92b945cc6a299207fc7a822b3256f645889101
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-03-05 19:01:32 -0700

    Array Methods

--- newPractice.js
+++ newPractice.js
@@ -2,6 +2,7 @@ const Sarah = {
     age: 29,
     height: `5'5"`,
     weight: 162,
+    desiredWeight: 130,
     isMale: false,
     favoriteFoods: [`Sushi`, `Bananas`, `Bacon`],
     address: {
@@ -10,4 +11,25 @@ const Sarah = {
         state:`Utah`
     }
 }
-Sarah.favoriteFoods[1];
+console.log(Sarah.favoriteFoods[1]);
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+let weightToLose = subtract(Sarah.weight, Sarah.desiredWeight);
+console.log(weightToLose);
+
+var foodIWantToEat = ["Caramel Popcorn", "Air Heads", "Raisin Bran"];
+
+foodIWantToEat.push("Sushi");
+foodIWantToEat.unshift("Prunes");
+foodIWantToEat.shift();
+var madeMeSick = foodIWantToEat.slice(0, 1);
+
+console.log(foodIWantToEat)
+console.log(madeMeSick)
+
+foodIWantToEat.splice(1, 0, "Maple Bars", "Caramels");
+
+console.log(foodIWantToEat);
